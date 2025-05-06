87cffd702a6119bfaf3451569649b28aaa503283
Abdullah-Jamal <abdullajamal699@gmail.com>
2025-05-06 10:21:02 +0300

    fix typeScript error

--- app/(root)/products/page.tsx
+++ app/(root)/products/page.tsx
@@ -2,8 +2,7 @@ import ProductContainer from '@/components/products/ProductContainer'
 import React from 'react'
 
 const products = async ({searchParams} : {searchParams : {layout?: string, search?: string}}) => {
-  const { layout = 'grid' } = await searchParams || {}
-  const { search } = await searchParams
+  const { layout = 'grid', search = '' } = searchParams || {};
   return (
       <ProductContainer layout = {layout} search = {search}/>
   )
