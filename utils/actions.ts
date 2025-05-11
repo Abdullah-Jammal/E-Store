9513e887da19aecd294aae22f1085afdfb229df6
Abdullah-Jamal <abdullajamal699@gmail.com>
2025-05-11 15:22:52 +0300

    add

--- utils/actions.ts
+++ utils/actions.ts
@@ -1,5 +1,7 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 "use server";
 import db from "@/utils/db";
+import { currentUser } from "@clerk/nextjs/server";
 import { redirect } from "next/navigation";
 
 // Fetch All Featured Products
@@ -36,14 +38,47 @@ export const fetchProductByID = async ({ id }: { id: string }) => {
       id: id,
     },
   });
-  if(!product) {
-    redirect('/products')
+  if (!product) {
+    redirect("/products");
   }
   return product;
 };
 
-
+// get user to check if the user logged in or not
+const getAuthUser = async () => {
+  const user = await currentUser();
+  if (!user) {
+    throw new Error("you must logein!");
+  }
+  return user;
+};
 // Create Product Function
-export const createProduct = async () : Promise<{message:string}> => {
-  return {message:"product created successfully!"}
-}
+export const createProduct = async (
+  prevState: any,
+  formData: FormData
+): Promise<{ message: string }> => {
+  const user = await getAuthUser();
+
+  try {
+    const name = formData.get("name") as string;
+    const description = formData.get("description") as string;
+    const featured = Boolean(formData.get("featured") as string);
+    const price = Number(formData.get("price") as string);
+    const image = formData.get("image") as File;
+
+    await db.products.create({
+      data: {
+        name,
+        description,
+        price,
+        featured,
+        image : "/images/product-1.jpg",
+        clerkId : user.id
+      }
+    })
+    return {message : "product created successfully!"}
+  } catch (error) {
+    return renderError
+    console.log(error);
+  }
+};
